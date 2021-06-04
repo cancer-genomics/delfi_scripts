489b46772b3e639a33dca852ee282fbcd6904e0c
Daniel Bruhm <danielbruhm@gmail.com>
2021-06-03 20:55:33 -0400

    Remove PlasmaTools dependency

--- z-scores/countReads.R
+++ z-scores/countReads.R
@@ -1,4 +1,11 @@
-library(PlasmaTools)
+library(Rsamtools)
+library(GenomicAlignments)
+library(GenomicRanges)
+library(matrixStats)
+library(GenomeInfoDb)
+library(data.table)
+library(dplyr)
+library(biovizBase)
 
 #----------------------------
 # Get command line arguments
@@ -14,7 +21,6 @@ outDir <- args[4]
 #---------------------------
 # Import reads into GRanges
 #------------------------------------------------------------------------------
-data("filters.hg19", package = "PlasmaTools")
 bins <- readRDS(binGR)
 bamfile <- file.path(bamdir, id)
 reads_dir <- file.path(outDir, "gal")
@@ -23,6 +29,19 @@ if (!dir.exists(reads_dir)){
 }
 sample <- gsub(".bam", "", id)
 reads_file <- file.path(reads_dir, paste0(sample, ".rds"))
+
+getReads <- function(bamfile, bamparams = NULL) {
+    if (is.null(bamparams)) {
+        bamparams <- ScanBamParam(flag = scanBamFlag(isDuplicate = FALSE,
+                                                 isSecondaryAlignment = FALSE,
+                                                 isUnmappedQuery = FALSE),
+                              mapqFilter = 30)
+    }
+    gal <- readGAlignments(bamfile, param = bamparams)
+    reads <- granges(gal)
+    return(reads)
+}
+
 if(!file.exists(reads_file)){
     original_reads <- getReads(bamfile)
     saveRDS(original_reads, reads_file)
@@ -33,6 +52,13 @@ if(!file.exists(reads_file)){
 #--------------
 # Filter reads
 #-----------------------------------------------------------
+load(<PATH TO filters.hg19.rda>)
+
+filterReads <- function(reads, filtered.regions) {
+    reads <- reads[!overlapsAny(reads, filtered.regions)]
+    return(reads)
+}
+
 filtered_reads <- filterReads(original_reads, filters.hg19)
 #-----------------------------------------------------------
 
@@ -40,6 +66,37 @@ filtered_reads <- filterReads(original_reads, filters.hg19)
 #------------------------
 # Count and GC-normalize
 #-------------------------------------------------------------
+gcCorrect <- function(granges) {
+    lower <- 0
+    upper <- quantile(granges$counts.mult, .99)
+    trend.points <- granges$counts.mult > lower & granges$counts.mult < upper
+    trend.counts <- granges[trend.points]$counts.mult
+    trend.gc <- granges[trend.points]$gc
+    num.sample.points <- min(length(trend.counts), 10E3L)
+    samp <- sample(1:length(trend.counts), num.sample.points)
+    # pad counts and GC
+    include <- c(which(bins$gc==min(bins$gc)), which(bins$gc==max(bins$gc)))
+    trend.counts <- c(granges[include]$counts.mult, trend.counts[samp])
+    trend.gc <- c(granges[include]$gc, trend.gc[samp])
+    initial.trend <- loess(trend.counts ~ trend.gc)
+    i <- seq(min(bins$gc), max(bins$gc), by = 0.001)
+    final.trend <- loess(predict(initial.trend, i) ~ i)
+    counts.pred <- predict(final.trend, granges$gc)
+    return(counts.pred)
+}
+
+countAndNormalize <- function(reads, bins) {
+    bins$counts <- countOverlaps(bins, reads)
+    ### normalize for filtered bases
+    ### weight is to get count per expected width of basepairs
+    weight <- (width(bins)[1]) / (width(bins) - bins$filtered.bases)
+    bins$counts.mult <- bins$counts * weight
+    bins$counts.mult <- log2(bins$counts.mult + 1)
+    bins$loess.pred <- gcCorrect(bins)
+    bins$adjusted <- bins$counts.mult - bins$loess.pred
+    return(bins)
+}
+
 normalized_counts <- countAndNormalize(filtered_reads, bins)
 counts_dir <- file.path(outDir, "binCounts")
 
--- z-scores/getScores.R
+++ z-scores/getScores.R
@@ -1,5 +1,11 @@
-library(PlasmaTools)
-library(ggplot2)
+library(Rsamtools)
+library(GenomicAlignments)
+library(GenomicRanges)
+library(matrixStats)
+library(GenomeInfoDb)
+library(data.table)
+library(dplyr)
+library(biovizBase)
 library(fs)
 
 #--------
@@ -10,7 +16,6 @@ dataDir <- "countReads.R"
 ## User must specify a plain text file providing the names of the healthy control files created by countReads.R
 ##
 healthyList <- "healthy-list.txt"
-binSize="50kb"
 #--------------------------------------------------------------------------------------------------
 
 binpath <- file.path(dataDir, "binCounts")
@@ -28,6 +33,42 @@ if (!dir.exists(file.path(dataDir, "scores"))) {
 }
 
 zscore.file <- file.path(dataDir, "scores", "zscores.rds")
+
+getArmMeans <- function(bincounts.list) {
+    arm_means.list <- lapply(1:length(bincounts.list), function(i){
+        b <- bincounts.list[[i]]
+        df <- data.table(arm = as.factor(b$arm), adjusted = b$adjusted)
+        df$adjusted <- df$adjusted - median(df$adjusted, na.rm = TRUE)
+        df <- df %>%
+            mutate(total = sum(adjusted)) %>%
+            group_by(arm) %>%
+            summarize(armmean = mean(adjusted))
+        df$armmean <- df$armmean - mean(df$armmean, na.rm = TRUE)
+        return(df$armmean)
+    })
+    return(arm_means.list)
+}
+
+getZscores <- function(bincounts.list, normal.indices, loo = FALSE) {
+    gr.list <- getArmMeans(bincounts.list)
+    zscores.list <- lapply(1:length(gr.list), function(i){
+        if (loo == FALSE | !(i %in% normal.indices)) {
+            keep <- normal.indices
+        } else {
+            keep <- setdiff(normal.indices, i)
+        }
+        normals <- gr.list[keep]
+        normals <- do.call("cbind", normals)
+        normals.mean <- rowMeans(as.matrix(normals))
+        normals.sd <- rowSds(as.matrix(normals))
+        zscores <- lapply(gr.list, function(gr){
+            (gr - normals.mean) / normals.sd
+        })
+        return(zscores[[i]])
+    })
+    return(zscores.list)
+}
+
 zscores.loo <- getZscores(bincounts, healthy.index, loo = TRUE)
 sample_names <- basename(binfiles)
 names(zscores.loo) <- gsub(".rds$", "", sample_names)
